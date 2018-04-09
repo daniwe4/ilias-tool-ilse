86c59dd9c9c607d9f6af92cb00c6ceb9f210f856
daniwe4 <daniel.weise@concepts-and-training.de>
2018-04-09 09:56:10 +0000

    space

--- ilse.php
+++ ilse.php
@@ -7,7 +7,7 @@
 require_once(__DIR__."/vendor/autoload.php");
 
 $app 		= new \CaT\Ilse\App\App();
-
+//
 // Do not run in unit testing context
 if (stripos($_SERVER["SCRIPT_NAME"], "phpunit") === false) {
 	$app->run();
